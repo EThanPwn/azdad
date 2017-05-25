6cf7f358d2648759236c0f8513363cb6f57e62cf
Unknown <minne.emeric@gmail.com>
2017-05-25 16:15:05 +0200

    delete

--- resources/shops/_ressource.lua
+++
@@ -1,9 +0,0 @@
-
-client_script 'keysControl.lua'
-client_script 'lib/menuClass.lua'
-client_script 'clothes/menu.lua'
-client_script 'barbers/menu.lua'
-client_script 'ammunations/menu.lua'
-
-server_script '../../essentialmode/config.lua'
-server_script 'process.lua'
