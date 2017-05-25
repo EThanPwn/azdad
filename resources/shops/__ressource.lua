d75c22fa52c281d671dbe2ee0d91b1e6c7eb6fe5
Unknown <minne.emeric@gmail.com>
2017-05-25 16:16:00 +0200

    add

---
+++ resources/shops/__ressource.lua
@@ -0,0 +1,9 @@
+
+client_script 'keysControl.lua'
+client_script 'lib/menuClass.lua'
+client_script 'clothes/menu.lua'
+client_script 'barbers/menu.lua'
+
+
+server_script '../../essentialmode/config.lua'
+server_script 'process.lua'
