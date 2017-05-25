e1159d621278dbe3a31ebccac8d23a4f46a1aa00
Unknown <minne.emeric@gmail.com>
2017-05-25 16:30:39 +0200

    precess lua

--- resources/shops/__ressource.lua
+++ resources/shops/__ressource.lua
@@ -1,9 +1,11 @@
+client_script {
+	"keysControl.lua"
+	"lib/menuClass.lua"
+	"clothes/menu.lua"
+	"barbers/menu.lua"
+}
 
-client_script 'keysControl.lua'
-client_script 'lib/menuClass.lua'
-client_script 'clothes/menu.lua'
-client_script 'barbers/menu.lua'
-
-
-server_script '../../essentialmode/config.lua'
-server_script 'process.lua'
+server_script {
+	"../../essentialmode/config.lua"
+	"process.lua"
+}
