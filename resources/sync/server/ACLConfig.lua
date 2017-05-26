fdf2bcb5fa090e0405e4d209a1ab0f0e2b253c17
Unknown <ethan.pwn@live.fr>
2017-05-26 02:31:07 +0200

    wl

--- resources/sync/server/ACLConfig.lua
+++ resources/sync/server/ACLConfig.lua
@@ -18,11 +18,17 @@ ACL.enablePlayerManagement = false
 
 -- The whitelist can handle IP's, Steam ID's and words
 ACL.whitelist = {
-	"steam:110000104615927",	--JackToshx
-	"steam:11000010691f003", --ChrisToshx
-	"steam:1100001119ca150", --Carry MacRoy
-	"steam:1100001190f80bd", --Bobby McRoy
-	"steam:11000010a0d6624" --Ken Kenaki
+	"steam:110000104615927", -- JackToshx
+	"steam:11000010691f003", -- ChrisToshx
+	"steam:1100001119ca150", -- Carry MacRoy
+	"steam:1100001190f80bd", -- Bobby McRoy
+	"steam:11000010a0d6624", -- Ken Kenaki
+	"steam:1100001040245a4", -- Rick johnson
+	"steam:110000104c5f34c", -- Zemog
+	"steam:11000010c6a536b", -- Yuri Sokolov	
+	"steam:11000011149aace",  -- biobu
+	"steam:11000010445a741" -- wreax
+	
 }
 
 -- Handles only IP's and Steam ID's
@@ -39,6 +45,9 @@ ACL.mods = {
 ACL.admins = {
 	--"ip:5.5.5.5",				-- Appoint this IP as administrator
 	--"steam:11000011badbabe"		-- Appoint this Steam ID as administrator
+	"steam:110000104615927", -- JackToshx
+	"steam:11000010691f003",-- ChrisToshx
+	"steam:11000010445a741" -- wreax
 }
 
 -- The banlist supersedes allow rules. Handles IP's, Steam ID's and words
