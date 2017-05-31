3f18a721ef0a0dc428a237e96297a55df1b0e866
Unknown <minne.emeric@gmail.com>
2017-05-31 15:32:38 +0200

    pv voip playerdrop test

--- resources/pv-voip/sv_voip.lua
+++ resources/pv-voip/sv_voip.lua
@@ -3,3 +3,8 @@ TriggerEvent('es:addCommand', 'v', function(source, args, user)
 	TriggerClientEvent('pv:voip', source, args[2])
 
 end)
+
+AddEventHandler('playerDropped', function()
+	TriggerClientEvent('pv:voip', source, args[2])
+
+end)
